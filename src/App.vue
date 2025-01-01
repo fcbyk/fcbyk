f18cf0d432e55f0943f1186e12d90eb78663907f
fcbyk <731240932@qq.com>
2025-01-01 15:26:01 +0800

    init: 初始化一个vue项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="centered">项目初始化</div>
+</template>
+
+<style lang="scss" scoped>
+.centered {
+  margin-top: 20vh;
+  text-align: center;
+}
+</style>
