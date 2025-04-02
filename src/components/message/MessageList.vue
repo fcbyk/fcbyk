48f84e9fb0f9e9e248a3e7b43f1a2b709a707b62
yoki <731240932@qq.com>
2025-04-02 21:42:29 +0800

    chore: 去除autoScroll Props

--- src/components/message/MessageList.vue
+++ src/components/message/MessageList.vue
@@ -11,38 +11,27 @@
       <MessageItem :role="message.role">
         <TextMessage :role="message.role" :content="message.content" :status="message.status" />
       </MessageItem>
-      
+
     </template>
   </div>
 </template>
 
 <script setup>
-import { ref, watch, nextTick,inject} from 'vue'
+import { ref, watch, nextTick, inject } from 'vue'
 import formatTime from '@/utils/formatTime'
 import chatConfig from '@/chat.config'
 import MessageItem from './MessageItem.vue'
 import TextMessage from './TextMessage.vue'
 
-const props = defineProps({
-  autoScroll: {
-    type: Boolean,
-    default: true
-  }
-})
-
 const messages = inject('messages')
 const container = ref(null)
 
+// 如果与前一条消息时间间隔超过阈值，则显示时间
 const shouldShowTime = (message, index) => {
   if (!message.time) return false
-
-  // 第一条消息总是显示时间
   if (index === 0) return true
-
   const prevMessage = messages.value[index - 1]
   if (!prevMessage.time) return false
-
-  // 如果与前一条消息时间间隔超过阈值，则显示时间
   return message.time - prevMessage.time > chatConfig.timeThreshold
 }
 
@@ -50,9 +39,7 @@ const shouldShowTime = (message, index) => {
 watch(
   () => messages.value,
   () => nextTick(() => {
-    // 自动滚动到底部
-    if (container.value && props.autoScroll)
-      container.value.scrollTop = container.value.scrollHeight
+    if (container.value) container.value.scrollTop = container.value.scrollHeight
   }),
   { deep: true })
 </script>
