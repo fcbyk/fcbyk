138c9cf015e142d9433dae4975db9b4eeda8bc93
Yoki <101487905+fcbyk@users.noreply.github.com>
2025-04-10 19:32:13 +0800

    Update action-menu.js

--- options/action-menu.js
+++ options/action-menu.js
@@ -2,7 +2,10 @@ import domain from "../qa/domain.js"
 import works from "../qa/works.js"
 import about from "../qa/about.js"
 
-const m = (label, action) => ({ label, action })
+const m = (label, action = {
+    question: createTextMsg(label),
+    answer: [createTextMsg("服务器繁忙，请稍后再试", 3000)]
+}) => ({ label, action, })
 
 export default [
     m("近期规划"),
