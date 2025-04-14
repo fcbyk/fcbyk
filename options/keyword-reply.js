cf9a7d6e98b5cbc738176bfa29d2c01d502617dc
Yoki <101487905+fcbyk@users.noreply.github.com>
2025-04-14 14:38:09 +0800

    Update keyword-reply.js

--- options/keyword-reply.js
+++ options/keyword-reply.js
@@ -1,8 +1,14 @@
 import createMsgCreator from "../utils/createMsgCreator.js"
 
 const text = createMsgCreator("text")
+const file = createMsgCreator("file")
 
 export const keywordReply = {
     "网易云": [text("音乐组件存在bug，等待修复中", 1000)],
-    "键盘测试": [text("下载组件还未写，还在开发中", 2000)]
+    "键盘测试": [file({
+            fileName: "KB2_105.exe",
+            fileSize: '645 KB',
+            fileType: "exe",
+            fileUrl: "https://github.com/fcbyk/fcbyk/releases/download/files/KB2_105.exe"
+        })]
 }
