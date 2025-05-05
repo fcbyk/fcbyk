1a6ecbbb31da979214ece347dbd10024f4200480
yoki <731240932@qq.com>
2025-05-06 03:35:49 +0800

    feat: 更新CDN配置，动态注入CSS样式

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { createPinia } from 'pinia'
 import { createHead } from '@vueuse/head'
 import App from './App.vue'
 import router from './router'
-    
+
+// 动态注入CCSS
+const cdnUrl = import.meta.env.VITE_APP_CDN_CSS
+if (cdnUrl) {
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = cdnUrl
+  document.head.appendChild(link)
+}
+
 const app = createApp(App)
 
 app.use(createPinia()).use(createHead()).use(router)
