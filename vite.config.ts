b71552c2026ad4490f9371f7211335632f3f9ba2
yoki <731240932@qq.com>
2025-05-06 02:43:04 +0800

    新增json数据文件生成

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   build: {
@@ -16,5 +20,16 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+  plugins: [{
+    name: 'generate-json',
+    closeBundle() {
+      // 导入配置
+      const configs = require('./dist/index.es.js');
+      // 将配置转换为 JSON
+      const jsonContent = JSON.stringify(configs.default, null, 2);
+      // 写入 JSON 文件
+      fs.writeFileSync(path.resolve(__dirname, 'dist/index.json'), jsonContent);
+    }
+  }]
 });
